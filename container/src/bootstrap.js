91e8a65ccfb7af5ce47fc1001caef84f3a6e73f9
Dani Lucaci <danilucaci@gmail.com>
2021-11-01 14:50:23 +0100

    build: add mount functions

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,2 +1,5 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as mountProducts } from "products/ProductsIndex";
+import { mount as mountCart } from "cart/CartShow";
+
+mountProducts(document.querySelector("#my-products"));
+mountCart(document.querySelector("#my-cart"));
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,22 @@
 import faker from "faker";
 
-let products = "";
+function mount(element) {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  element.innerHTML = products;
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+if (process.env.NODE_ENV === "development") {
+  const element = document.querySelector("#dev-products");
+
+  if (element) {
+    mount(element);
+  }
+}
+
+export { mount };
