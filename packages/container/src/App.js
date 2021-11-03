390e76286d084a53e54f580b8e24b5a8b4065d5b
Dani Lucaci <danilucaci@gmail.com>
2021-11-03 12:33:15 +0100

    feat: add Progress component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const AuthApp = lazy(() => import("./components/AuthApp"));
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -18,10 +19,12 @@ export default () => {
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Header />
-        <Switch>
-          <Route path="/auth" component={AuthApp} />
-          <Route exact path="/" component={MarketingApp} />
-        </Switch>
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route exact path="/" component={MarketingApp} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </StylesProvider>
   );
