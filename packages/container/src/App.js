14bf6d7f356902e81fa79b4918aa7f4d42745276
Dani Lucaci <danilucaci@gmail.com>
2021-11-03 09:38:20 +0100

    fix: add css prefixes for css clashes between apps

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <div>
-      <h1>hello</h1>
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
